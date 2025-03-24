3b58ffd5e8af8814351e7d6ece989ad01778e759
jubick1337 <mattyson.so@gmail.com>
2025-03-24 08:31:31 -0700

    Add overlong buffer configuration and max response length to validation reward function

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -238,7 +238,7 @@ def main_task(config, compute_score=None):
 
     # Turn off num_examine, context length too long
     if config.trainer.get('run_validation', True):
-        val_reward_fn = reward_manager_cls(tokenizer=tokenizer, num_examine=0, compute_score=compute_score)
+        val_reward_fn = reward_manager_cls(tokenizer=tokenizer, num_examine=0, compute_score=compute_score, overlong_buffer_cfg=config.reward_model.reward_manager.get('overlong_buffer', None), max_resp_length=config.data.max_response_length)
     else:
         val_reward_fn = None
 
